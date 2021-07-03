3fde31d1bffd183e3e5983307a0b3f1155c3f5a8
at130722 <53244359+at130722@users.noreply.github.com>
2021-07-03 19:20:22 +0700

    update

---
+++ mainprocess.js
@@ -0,0 +1,27 @@
+function login() {
+	console.log("login func");
+	var url = "http://apii.xn--kinqt-isa.vn:3069/api/login";
+	var data = {
+		'username': document.getElementById("username").value,
+		'password': document.getElementById("passwd").value
+	};
+	console.log(data);
+    var json = JSON.stringify(data);
+        console.log(url)
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.onload = function() {
+            var resp = JSON.parse(xhr.responseText);
+            if (xhr.readyState == 4 && xhr.status == "200") {
+                console.log(resp);
+                if (resp.status==0) {
+                } else {
+                    document.getElementById("notify").innerHTML = "Thông tin đăng nhập không chính xác!!!";
+                }
+            } else {
+                document.getElementById("notify").innerHTML = "Đăng nhập thất bại!!!";
+            }
+        }
+        xhr.send(json);
+}
